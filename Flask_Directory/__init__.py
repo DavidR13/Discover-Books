d813d3822fd0b1d3d76712a36deac4fdc698414d
David Rojas <70168138+DavidR13@users.noreply.github.com>
2022-01-09 14:09:00 -0500

    changed storage of keys

--- Flask_Directory/__init__.py
+++ Flask_Directory/__init__.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask
+from Flask_Directory import json_functions
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
+
+abs_path = os.environ['ABS_PATH']
+secret_key_file = json_functions.read_from_file(f'{abs_path}/keys.json')
+secret_key = secret_key_file["SECRET_KEY"]
+
+app.config["SECRET_KEY"] = secret_key
 
 from Flask_Directory import routes
--- Flask_Directory/api_methods.py
+++ Flask_Directory/api_methods.py
@@ -1,23 +1,19 @@
 import requests
-import os
 from Flask_Directory import json_functions
+import os
 
 
 def request_key():
-    api_key = os.environ['API_KEY']
-    return api_key
-
-
-def get_abs_path():
     abs_path = os.environ['ABS_PATH']
-    return abs_path
+    api_key_file = json_functions.read_from_file(f'{abs_path}/keys.json')
+    api_key = api_key_file["API_KEY"]
+    return api_key
 
 
 def request_best_sellers(date, name):
     best_sellers_url = "https://api.nytimes.com/svc/books/v3/lists/" + date + "/" + name + ".json?api-key=" + request_key()
     best_sellers_response = requests.get(best_sellers_url).json()
 
-    json_functions.save_to_file(best_sellers_response, f"{get_abs_path()}/best_sellers.json")
     return best_sellers_response
 
 
@@ -25,7 +21,6 @@ def request_book_reviews_isbn(isbn):
     reviews_isbn_url = "https://api.nytimes.com/svc/books/v3/reviews.json?isbn=" + str(isbn) + "&api-key=" + request_key()
     reviews_isbn_response = requests.get(reviews_isbn_url).json()
 
-    json_functions.save_to_file(reviews_isbn_response, f"{get_abs_path()}/isbn_reviews.json")
     return reviews_isbn_response
 
 
@@ -33,7 +28,6 @@ def request_book_reviews_title(title):
     reviews_title_url = "https://api.nytimes.com/svc/books/v3/reviews.json?title=" + title + "&api-key=" + request_key()
     reviews_title_response = requests.get(reviews_title_url).json()
 
-    json_functions.save_to_file(reviews_title_response, f"{get_abs_path()}/title_reviews.json")
     return reviews_title_response
 
 
@@ -41,5 +35,4 @@ def request_book_reviews_author(author):
     reviews_author_url = "https://api.nytimes.com/svc/books/v3/reviews.json?author=" + author + "&api-key=" + request_key()
     reviews_author_response = requests.get(reviews_author_url).json()
 
-    json_functions.save_to_file(reviews_author_response, f"{get_abs_path()}/author_reviews.json")
     return reviews_author_response
